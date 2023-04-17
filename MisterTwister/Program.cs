d6113c212e388b1bf28dd5faf7cf5a00c375c67a
shadoko7 <arudawska7@gmail.com>
2023-04-17 20:44:14 +0200

    Create Main method

---
+++ MisterTwister/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MisterTwister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I'm Mister Twister");
+            Thread.Sleep(1000);
+            Console.WriteLine("If want to play Twister with me press s");
+            Thread.Sleep(1000);
+            Console.WriteLine("... or press h to get help");
+            Thread.Sleep(1000);
+            Console.WriteLine("... or press q to quit");
+
+            var key = Console.ReadKey();
+
+            switch (key.Key)
+            {
+                case ConsoleKey.S:
+                    // start game
+                    break;
+                case ConsoleKey.H:
+                    // display help
+                    break;
+                case ConsoleKey.Q:
+                    // quit app
+                    break;
+                default:
+                    // wait for valid input
+                    break;
+            }
+        }
+    }
+}
